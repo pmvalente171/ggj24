7047c909685a36be7bcfdb33af37a5d4804a07bf
diogo <dc.tavares@campus.fct.unl.pt>
2024-01-27 10:30:50 +0000

    momentum

--- Assets/Scripts/Player/PlayerMov.cs
+++ Assets/Scripts/Player/PlayerMov.cs
@@ -1,65 +1,65 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMov : MonoBehaviour
 {
     [SerializeField] private float startRotationSpeed = 10f;
     [SerializeField] private float rotationSpeed = 4f;
-    [SerializeField] private Transform LeftFoot;
-    [SerializeField] private Transform RightFoot;
-    
+    [SerializeField] private Transform leftFoot;
+    [SerializeField] private Transform rightFoot;
+    [SerializeField] private float momentumDecayRate = 0.95f;
+
     private Rigidbody _rigidbody;
-    private float _leftFootRotation;
-    private float _rightFootRotation;
-    
-    // Start is called before the first frame update
+    private float _leftFootMomentum;
+    private float _rightFootMomentum;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.D))
-        {
-            _leftFootRotation = 0f;
-        }
-        
-        if (Input.GetKeyUp(KeyCode.O) || Input.GetKeyUp(KeyCode.K))
-        {
-            _rightFootRotation = 0f;
-        }
-        
+        HandleInput();
+        ApplyMomentumDecay();
+    }
+
+    private void HandleInput()
+    {
         if (Input.GetKey(KeyCode.W))
         {
-            _leftFootRotation -= startRotationSpeed * Time.deltaTime;
-            _leftFootRotation = Mathf.Clamp(_leftFootRotation, -1, 1);
-            return;
-        }
-        if (Input.GetKey(KeyCode.O))
-        {
-            _rightFootRotation += startRotationSpeed * Time.deltaTime;
-            _rightFootRotation = Mathf.Clamp(_rightFootRotation, -1, 1);
-            return;
+            _leftFootMomentum -= startRotationSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            _leftFootRotation += startRotationSpeed * Time.deltaTime;
-            _leftFootRotation = Mathf.Clamp(_leftFootRotation, -1, 1);
-            return;
+            _leftFootMomentum += startRotationSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.O))
+        {
+            _rightFootMomentum += startRotationSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.K))
         {
-            _rightFootRotation -= startRotationSpeed * Time.deltaTime;
-            _rightFootRotation = Mathf.Clamp(_rightFootRotation, -1, 1);
-            return;
+            _rightFootMomentum -= startRotationSpeed * Time.deltaTime;
         }
+
+        _leftFootMomentum = Mathf.Clamp(_leftFootMomentum, -1, 1);
+        _rightFootMomentum = Mathf.Clamp(_rightFootMomentum, -1, 1);
     }
-    
-    // Function to rotate rigidbody around a point
+
+    private void ApplyMomentumDecay()
+    {
+        // Gradually reduce the momentum over time if no input is provided
+        _leftFootMomentum *= momentumDecayRate;
+        _rightFootMomentum *= momentumDecayRate;
+    }
+
+    private void FixedUpdate()
+    {
+        RotateAroundPoint(_rigidbody, leftFoot.position, Vector3.up, _leftFootMomentum * rotationSpeed);
+        RotateAroundPoint(_rigidbody, rightFoot.position, Vector3.up, _rightFootMomentum * rotationSpeed);
+    }
+
     private void RotateAroundPoint(Rigidbody rigidbody, Vector3 point, Vector3 axis, float angle)
     {
         Quaternion rotation = Quaternion.AngleAxis(angle, axis);
@@ -67,13 +67,5 @@ public class PlayerMov : MonoBehaviour
         Vector3 rotatedPointToFoot = rotation * pointToFoot;
         rigidbody.position = point + rotatedPointToFoot;
         rigidbody.rotation *= rotation;
-    }
-    
-    private void FixedUpdate()
-    {
-        // Rotate the left foot
-        RotateAroundPoint(_rigidbody, LeftFoot.position, Vector3.up, _leftFootRotation * rotationSpeed);
-        // Rotate the right foot
-        RotateAroundPoint(_rigidbody, RightFoot.position, Vector3.up, _rightFootRotation * rotationSpeed);
     }
 }
