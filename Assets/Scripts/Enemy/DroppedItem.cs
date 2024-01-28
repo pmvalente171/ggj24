1f630c5c16d984fb0cc166958e5f292d11ca0926
diogo <dc.tavares@campus.fct.unl.pt>
2024-01-28 12:00:10 +0000

    merg

---
+++ Assets/Scripts/Enemy/DroppedItem.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroppedItem : MonoBehaviour, IDamageable
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player touched the dropped item!");
+            Destroy(this.gameObject);
+        }
+        else if (other.GetComponent<Enemy>() != null)
+        {
+            Debug.Log("Enemy picked up the item!");
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Destroy(this.gameObject);
+    }
+}
+    
